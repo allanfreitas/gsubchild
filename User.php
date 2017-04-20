aea0863dc638fadc5e26b96915ee6357a717f668
Allan Freitas <allanfreitasci@gmail.com>
2017-04-20 13:36:24 -0300

    add tld

--- User.php
+++ User.php
@@ -7,11 +7,13 @@ class User
 {
   public $username;
   public $email;
+  public $tld;
 
   public function __construct($username)
   {
     $this->username = $username;
     $this->email = $username.'@somemail.com';
+    $this->tld = 'com';
   }
 
   public function run()
