40e0b5c064760eadc30b561c709ab6c18bdc07b5
Allan Freitas <allanfreitasci@gmail.com>
2017-04-20 18:23:01 -0300

    to new development branch

--- User.php
+++ User.php
@@ -8,12 +8,14 @@ class User
   public $username;
   public $email;
   public $tld;
+  public $password;
 
   public function __construct($username)
   {
     $this->username = $username;
     $this->email = $username.'@somemail.com';
     $this->tld = 'com';
+    $this->passwrod = 'none-of-your-bussiness';
   }
 
   public function run()
